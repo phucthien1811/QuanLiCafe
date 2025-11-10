036186205cd42376bcfcec827a8fa0a216dc0c88
Thien Tiy <jack0baohbocon@gmail.com>
2025-11-10 17:09:01 +0700

    sửa lại từ code tay thành giao diện kéo thả form login

---
+++ QuanLiCafe/Forms/FormLogin.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace QuanLiCafe.Forms
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelHeader = new Panel();
+            lblTitle = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label1 = new Label();
+            txbUserName = new TextBox();
+            panel3 = new Panel();
+            txbPassword = new TextBox();
+            label2 = new Label();
+            btnLogin = new Button();
+            btnExit = new Button();
+            panelHeader.SuspendLayout();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            panelHeader.BackColor = Color.FromArgb(52, 73, 94);
+            panelHeader.Controls.Add(lblTitle);
+            panelHeader.Dock = DockStyle.Top;
+            panelHeader.Location = new Point(0, 0);
+            panelHeader.Margin = new Padding(3, 4, 3, 4);
+            panelHeader.Name = "panelHeader";
+            panelHeader.Size = new Size(548, 133);
+            panelHeader.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            lblTitle.Font = new Font("Segoe UI", 24F, FontStyle.Bold);
+            lblTitle.ForeColor = Color.White;
+            lblTitle.Location = new Point(11, 13);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(549, 107);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "☕ LOGIN";
+            lblTitle.TextAlign = ContentAlignment.MiddleCenter;
+            lblTitle.Click += lblTitle_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnExit);
+            panel1.Controls.Add(btnLogin);
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(12, 157);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(523, 225);
+            panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(txbUserName);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(3, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(517, 58);
+            panel2.TabIndex = 1;
+            panel2.Paint += panel2_Paint;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(5, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(159, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Tên đăng nhập:";
+            // 
+            // txbUserName
+            // 
+            txbUserName.Location = new Point(177, 14);
+            txbUserName.Name = "txbUserName";
+            txbUserName.Size = new Size(327, 27);
+            txbUserName.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(txbPassword);
+            panel3.Controls.Add(label2);
+            panel3.Location = new Point(3, 84);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(517, 58);
+            panel3.TabIndex = 2;
+            // 
+            // txbPassword
+            // 
+            txbPassword.Location = new Point(177, 14);
+            txbPassword.Name = "txbPassword";
+            txbPassword.Size = new Size(327, 27);
+            txbPassword.TabIndex = 1;
+            txbPassword.UseSystemPasswordChar = true;
+            txbPassword.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(5, 13);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 24);
+            label2.TabIndex = 0;
+            label2.Text = "Mật khẩu:";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(31, 167);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(136, 35);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Đăng nhập";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += button1_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(350, 167);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(136, 35);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "Thoát";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += button2_Click;
+            // 
+            // FormLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(548, 391);
+            Controls.Add(panel1);
+            Controls.Add(panelHeader);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Margin = new Padding(3, 4, 3, 4);
+            MaximizeBox = false;
+            Name = "FormLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Đăng Nhập";
+            panelHeader.ResumeLayout(false);
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblTitle;
+        private Panel panel1;
+        private Panel panel2;
+        private TextBox txbUserName;
+        private Label label1;
+        private Panel panel3;
+        private TextBox txbPassword;
+        private Label label2;
+        private Button btnLogin;
+        private Button btnExit;
+    }
+}
--- QuanLiCafe/Forms/FormLogin.cs
+++ QuanLiCafe/Forms/FormLogin.cs
@@ -1,6 +1,7 @@
 using QuanLiCafe.Data;
 using QuanLiCafe.Models;
 using QuanLiCafe.Services;
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -10,12 +11,6 @@ namespace QuanLiCafe.Forms
     {
         private readonly CafeContext _context;
         private readonly AuthService _authService;
-        
-        private TextBox txtUsername = null!;
-        private TextBox txtPassword = null!;
-        private Button btnLogin = null!;
-        private Button btnExit = null!;
-        private CheckBox chkShowPassword = null!;
 
         public User? LoggedInUser { get; private set; }
 
@@ -26,144 +21,32 @@ namespace QuanLiCafe.Forms
             InitializeComponent();
         }
 
-        private void InitializeComponent()
+        // ========== EVENT HANDLERS FROM DESIGNER ==========
+
+        private void lblTitle_Click(object sender, EventArgs e)
         {
-            this.Text = "Login - Cafe Management";
-            this.Size = new Size(500, 400);
-            this.StartPosition = FormStartPosition.CenterScreen;
-            this.BackColor = Color.White;
-            this.FormBorderStyle = FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-
-            // Header Panel
-            var panelHeader = new Panel
-            {
-                Dock = DockStyle.Top,
-                Height = 100,
-                BackColor = Color.FromArgb(52, 73, 94)
-            };
-
-            var lblTitle = new Label
-            {
-                Text = "? LOGIN",
-                Font = new Font("Segoe UI", 24, FontStyle.Bold),
-                ForeColor = Color.White,
-                AutoSize = false,
-                Size = new Size(480, 80),
-                Location = new Point(10, 10),
-                TextAlign = ContentAlignment.MiddleCenter
-            };
-            panelHeader.Controls.Add(lblTitle);
-
-            // Main Panel
-            var panelMain = new Panel
-            {
-                Location = new Point(50, 130),
-                Size = new Size(400, 200),
-                BackColor = Color.White
-            };
-
-            var lblUsername = new Label
-            {
-                Text = "?? Username:",
-                Location = new Point(0, 0),
-                Size = new Size(400, 30),
-                Font = new Font("Segoe UI", 12, FontStyle.Bold),
-                ForeColor = Color.FromArgb(52, 73, 94)
-            };
-
-            txtUsername = new TextBox
-            {
-                Location = new Point(0, 35),
-                Size = new Size(400, 35),
-                Font = new Font("Segoe UI", 12),
-                BorderStyle = BorderStyle.FixedSingle
-            };
-
-            var lblPassword = new Label
-            {
-                Text = "?? Password:",
-                Location = new Point(0, 80),
-                Size = new Size(400, 30),
-                Font = new Font("Segoe UI", 12, FontStyle.Bold),
-                ForeColor = Color.FromArgb(52, 73, 94)
-            };
-
-            txtPassword = new TextBox
-            {
-                Location = new Point(0, 115),
-                Size = new Size(400, 35),
-                Font = new Font("Segoe UI", 12),
-                BorderStyle = BorderStyle.FixedSingle,
-                UseSystemPasswordChar = true
-            };
-
-            chkShowPassword = new CheckBox
-            {
-                Text = "Show password",
-                Location = new Point(0, 155),
-                Size = new Size(150, 25),
-                Font = new Font("Segoe UI", 10)
-            };
-            chkShowPassword.CheckedChanged += (s, e) =>
-            {
-                txtPassword.UseSystemPasswordChar = !chkShowPassword.Checked;
-            };
-
-            panelMain.Controls.AddRange(new Control[] {
-                lblUsername, txtUsername, lblPassword, txtPassword, chkShowPassword
-            });
-
-            // Buttons
-            btnLogin = new Button
-            {
-                Text = "?? LOGIN",
-                Location = new Point(50, 300),
-                Size = new Size(180, 50),
-                BackColor = Color.FromArgb(46, 204, 113),
-                ForeColor = Color.White,
-                Font = new Font("Segoe UI", 14, FontStyle.Bold),
-                FlatStyle = FlatStyle.Flat,
-                Cursor = Cursors.Hand
-            };
-            btnLogin.FlatAppearance.BorderSize = 0;
-            btnLogin.Click += BtnLogin_Click;
-            btnLogin.MouseEnter += (s, e) => btnLogin.BackColor = Color.FromArgb(39, 174, 96);
-            btnLogin.MouseLeave += (s, e) => btnLogin.BackColor = Color.FromArgb(46, 204, 113);
-
-            btnExit = new Button
-            {
-                Text = "?? EXIT",
-                Location = new Point(270, 300),
-                Size = new Size(180, 50),
-                BackColor = Color.FromArgb(231, 76, 60),
-                ForeColor = Color.White,
-                Font = new Font("Segoe UI", 14, FontStyle.Bold),
-                FlatStyle = FlatStyle.Flat,
-                Cursor = Cursors.Hand
-            };
-            btnExit.FlatAppearance.BorderSize = 0;
-            btnExit.Click += (s, e) => Application.Exit();
-            btnExit.MouseEnter += (s, e) => btnExit.BackColor = Color.FromArgb(192, 57, 43);
-            btnExit.MouseLeave += (s, e) => btnExit.BackColor = Color.FromArgb(231, 76, 60);
-
-            this.Controls.Add(panelHeader);
-            this.Controls.Add(panelMain);
-            this.Controls.Add(btnLogin);
-            this.Controls.Add(btnExit);
-
-            // Enter key to login
-            this.AcceptButton = btnLogin;
+            // Empty event handler
         }
 
-        private void BtnLogin_Click(object? sender, EventArgs e)
+        private void panel2_Paint(object sender, PaintEventArgs e)
         {
-            var username = txtUsername.Text.Trim();
-            var password = txtPassword.Text;
+            // Empty event handler
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Empty event handler
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ??NG NH?P button
+            var username = txbUserName.Text.Trim();
+            var password = txbPassword.Text;
 
             if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
             {
-                MessageBox.Show("Please enter all information!", "?? Warning",
+                MessageBox.Show("Vui lòng nh?p ??y ?? thông tin!", "?? C?nh Báo",
                     MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
@@ -174,10 +57,10 @@ namespace QuanLiCafe.Forms
 
                 if (user == null)
                 {
-                    MessageBox.Show("Invalid username or password!", "? Login Failed",
+                    MessageBox.Show("Tên ??ng nh?p ho?c m?t kh?u không ?úng!", "? ??ng Nh?p Th?t B?i",
                         MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtPassword.Clear();
-                    txtPassword.Focus();
+                    txbPassword.Clear();
+                    txbPassword.Focus();
                     return;
                 }
 
@@ -187,9 +70,15 @@ namespace QuanLiCafe.Forms
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Login error:\n{ex.Message}", "? Error",
+                MessageBox.Show($"L?i ??ng nh?p:\n{ex.Message}", "? L?i",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // THOÁT button
+            Application.Exit();
         }
     }
 }
